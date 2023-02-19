d454b14d6aa98d455d7c714d33e57f2de06fd8dd
Omar-Mohamed-1 <122884233+Omar-Mohamed-1@users.noreply.github.com>
2023-02-20 00:39:56 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,9 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 puts("\"Programming is like building a multilingual puzzle");
-	
+
 return (0);
-=======
 	puts("\"Programming is like building a multilingual puzzle");	
 	return (0);
->>>>>>> 69f1c31ec14f0a529451bd023e8f74ae907cd155
 }
