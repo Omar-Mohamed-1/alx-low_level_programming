d10f23457610e2fa27659b9bab6e0bae241c2549
Omar-Mohamed-1 <om725569@gmail.com>
2023-02-20 07:18:22 -0800

    6666

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+ * main -Entry point
+ *
+ * return: Always 0 (success)
+ */
 int main(void)
 {
-	/*this was an entry point*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
