644f666c805b11a66ad5b7007ecb2ee6eb905fa8
Omar-Mohamed-1 <om725569@gmail.com>
2023-02-20 07:05:35 -0800

    4-task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
-/*
- * main --> prints "Programming is like building a multilingual puzzle
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-	puts("\"Programming is like building a multilingual puzzle");	
-	return(0);
-}
+int main(void){
+	/*this was an entry point*/
+	puts(""Programming is like building a multilingual puzzle");
+	retirn(0);}
