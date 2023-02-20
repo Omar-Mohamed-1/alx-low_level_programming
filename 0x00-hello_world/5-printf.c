7767259b51bc8c1dbc8deddc8bf623ae48a2920a
Omar-Mohamed-1 <om725569@gmail.com>
2023-02-20 07:35:20 -0800

    fifths task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Dscription: print with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
